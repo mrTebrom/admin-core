d70f16e262f8311a9b401f3ecb2d86cda082a6d1
mrTebrom <mr.tebron@gmail.com>
2024-06-21 04:19:18 +0500

    default router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = createBrowserRouter([]);
 function App() {
-  return <div className="App"></div>;
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
